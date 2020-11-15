7ed334b2f3e9265e9a1441007f3cfe31c62f2ecc
Zsanna Csengeri <72246935+zsannacsengeri@users.noreply.github.com>
2020-11-15 19:51:33 +0000

    Add files via upload

---
+++ cirkle.py
@@ -0,0 +1,10 @@
+import math
+
+print("This program calculate the area and circumsference of a cirkle.")
+
+radius = float( input("Please type the radius:"))
+
+print("Area of the circle:",round(math.pi *radius*radius,2))
+print("Circumference of the cirkle",round(2*math.pi*radius,2))
+
+           
